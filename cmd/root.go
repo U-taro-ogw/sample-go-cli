6387aef3b044cbf4014bcfe87f64d7bda211958a
U-taro-ogw <yuutarou0406@gmail.com>
2020-05-10 02:36:56 +0900

    show

--- cmd/root.go
+++ cmd/root.go
@@ -26,42 +26,34 @@ import (
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "sample-go-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+func NewCmdRoot() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "sample-go-cli",
+		Short: "A brief description of your application",
+	}
+	cobra.OnInitialize(initConfig)
+	cmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.sample-go-cli.yaml)")
+	cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	cmd.AddCommand(NewCmdShow())
+	return cmd
 }
 
+
+
 // Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+// This is called by main.main(). It only needs to happen once to the RootCmd.
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+	cmd := NewCmdRoot()
+	cmd.SetOutput(os.Stdout)
+	if err := cmd.Execute(); err != nil {
+		cmd.SetOutput(os.Stdout)
+		cmd.Println(err)
 		os.Exit(1)
 	}
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.sample-go-cli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
---
+++ cmd/show.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func NewCmdShow() *cobra.Command {
+	type Options struct {
+		optint int
+		optstr string
+	}
+
+	var (
+		o = &Options{}
+	)
+
+	cmd := &cobra.Command{
+		Use:   "show",
+		Short: "A brief description of your command",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Printf("show called: optint: %d, optstr: %s", o.optint, o.optstr)
+		},
+	}
+	cmd.Flags().IntVarP(&o.optint, "int", "i", 0, "int option")
+	cmd.Flags().StringVarP(&o.optstr, "str", "s", "default", "string option")
+
+	return cmd
+}
+
+func init() {
+}
