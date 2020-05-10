5b078a0317bfe0ab22a1f2445f8b09c00e748638
U-taro-ogw <yuutarou0406@gmail.com>
2020-05-10 20:00:42 +0900

    create validate

--- cmd/show_test.go
+++ cmd/show_test.go
@@ -10,10 +10,15 @@ func TestShow(t *testing.T) {
 	cases := []struct {
 		command string
 		want    string
+		errWant string
 	}{
-		{command: "sample-go-cli show", want: "show called: optint: 0, optstr: default"},
-		{command: "sample-go-cli show --int 10", want: "show called: optint: 10, optstr: default"},
+		{command: "sample-go-cli show", want: "", errWant: "Parameter error: Optstr is required"},
+		{command: "sample-go-cli show --int 10", want: "", errWant: "Parameter error: Optstr is required"},
 		{command: "sample-go-cli show --str test", want: "show called: optint: 0, optstr: test"},
+		{command: "sample-go-cli show --str \"test1 test2\"", want: "", errWant: "Parameter error: Optstr is not valid test1 test2"},
+		{command: "sample-go-cli show --int 1000 --str 123", want: "", errWant: "Parameter error: Optint cannot be greater than 10"},
+		{command: "sample-go-cli show --int -1 --str abc", want: "", errWant: "Parameter error: Optint must be greater than 0"},
+		{command: "sample-go-cli show --int 1 --str abc", want: "show called: optint: 1, optstr: abc", errWant: ""},
 	}
 
 	for _, c := range cases {
@@ -22,7 +27,12 @@ func TestShow(t *testing.T) {
 		cmd.SetOutput(buf)
 		cmdArgs := strings.Split(c.command, " ")
 		cmd.SetArgs(cmdArgs[1:])
-		cmd.Execute()
+
+		if err := cmd.Execute(); err != nil {
+			if c.errWant != err.Error() {
+				t.Errorf("unexpected error response: errWant:%+v, get:%+v", c.errWant, err.Error())
+			}
+		}
 
 		get := buf.String()
 		if c.want != get {
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.13
 
 require (
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mitchellh/mapstructure v1.3.0 // indirect
 	github.com/pelletier/go-toml v1.7.0 // indirect
@@ -14,5 +16,6 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.7.0
 	golang.org/x/sys v0.0.0-20200509044756-6aff5f38e54f // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0
 	gopkg.in/ini.v1 v1.55.0 // indirect
 )
